81bd5f753e4fd2acfa1b21bbbe6d2daa9ef6b7b7
Bruno Pimentel <hello@bruno.so>
2026-02-13 11:19:19 -0300

    chore: add function for automatically generating profile info

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,9 @@ type HeaderProps = {
   name: string
   photoUrl: string
   bio: string
-  role: string
 }
 
-export default function Header({ name, photoUrl, bio, role }: HeaderProps) {
+export default function Header({ name, photoUrl, bio }: HeaderProps) {
   return (
     <div className="flex flex-col items-center gap-4">
       <img
@@ -17,12 +16,9 @@ export default function Header({ name, photoUrl, bio, role }: HeaderProps) {
       <h1 className="max-w-md text-center text-4xl font-bold tracking-tight text-zinc-900 dark:text-white sm:text-3xl">
         {name}
       </h1>
-      <h2 className="max-w-md text-center text-xl font-medium text-zinc-600 dark:text-zinc-400 sm:text-lg">
-        {role}
-      </h2>
-      <p className="max-w-md text-center text-lg text-zinc-600 dark:text-zinc-300 sm:text-md">
+      <h3 className="max-w-md text-center text-lg text-zinc-600 dark:text-zinc-300 sm:text-md">
         {bio}
-      </p>
+      </h3>
     </div>
   )
 }
